b27bffab8bcea361d44dac7e8e1e8ed6c16c4d88
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-30 17:45:17 -0300

    feat: salesModel.getAll test implemented

---
+++ test/unit/models/salesModel.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+
+describe('O endpoint `/sales`', () => {
+  const salesMock = [
+    { id: 1, date: "2022-03-30 16:39:09" },
+    { id: 2, date: "2022-03-30 16:39:09" }
+  ];
+
+  before(() => {
+    const execute = [salesMock];
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
+  it('retorna todas as vendas cadastradas no DB', async () => {
+    const result = await salesModel.getAll();
+
+    expect(result).to.be.an('array');
+    expect(result).to.have.length(2);
+    expect(result).to.equal(salesMock);
+  });
+});
