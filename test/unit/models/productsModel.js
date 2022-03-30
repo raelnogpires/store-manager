141b1b85ce907c89099e8bb80f9fbd63302c4746
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-30 16:58:33 -0300

    feat: getAll products test implemented

---
+++ test/unit/models/productsModel.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+const productsModel = require('../../../models/productsModel');
+const productsMock = require('./productsMock.json');
+
+describe('O endpoint `/products`', () => {
+  before(() => {
+    const execute = (productsMock);
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
+  it('lista todos os produtos cadastrados no DB', async () => {
+    const result = await productsModel.getAll();
+
+    expect(result).to.equal(productsMock);
+  });
+});
