c79e44a8265a9334a4937fdc44ebff526f1a017f
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-31 15:04:33 -0300

    feat: validation middleware for POST /products created

---
+++ middlewares/productsValidation.js
@@ -0,0 +1,58 @@
+const BAD_REQUEST = 400;
+const UNPROCESSABLE_ENTITY = 422;
+
+const nameExist = (name) => {
+  if (name === undefined || typeof name !== 'string' || name.length === 0) {
+    return false;
+  }
+  return true;
+};
+
+const nameLength = (name) => {
+  if (name.length < 5) return false;
+  return true;
+};
+
+const nameValidation = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!nameExist(name)) {
+    return res.status(BAD_REQUEST).json({ message: '"name" is required' });
+  }
+
+  if (!nameLength(name)) {
+    return res.status(UNPROCESSABLE_ENTITY).json(
+      { message: '"name" length must be at least 5 characters long' },
+    );
+  }
+
+  next();
+};
+
+const quantityExist = (quantity) => {
+  if (quantity === undefined || typeof quantity !== 'number') return false;
+  return true;
+};
+
+const quantityValid = (quantity) => {
+  if (quantity <= 0 || !Number.isInteger(quantity)) return false;
+  return true;
+};
+
+const quantityValidation = (req, res, next) => {
+  const { quantity } = req.body;
+
+  if (!quantityExist(quantity)) {
+    return res.status(BAD_REQUEST).json({ message: '"quantity" is required' });
+  }
+
+  if (!quantityValid(quantity)) {
+    return res.status(UNPROCESSABLE_ENTITY).json(
+      { message: '"quantity" must be greater than or equal to 1' },
+    );
+  }
+
+  next();
+};
+
+module.exports = { nameValidation, quantityValidation };
