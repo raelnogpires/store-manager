76a579fb8a5ea82a2c44dc48c38a6ad07e159323
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-30 21:09:55 -0300

    feat: productsController methods implemented

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,8 +1,29 @@
-// const productsModel = require('../models/productsModel');
+const productsService = require('../services/productsService');
 
-// const HTTP_OK = 200;
-// const INTERNAL_ERROR = 500;
+const HTTP_OK = 200;
+const INTERNAL_ERROR = 500;
+const NOT_FOUND = 404;
 
-// const getAll = async (_req, res) => {};
+const getAll = async (_req, res) => {
+  const result = await productsService.getAll();
 
-// module.exports = { getAll };
+  if (!result) {
+    return res.status(INTERNAL_ERROR).json({ message: 'Erro no Servidor' });
+  }
+
+  return res.status(HTTP_OK).json(result);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await productsService.getById(id);
+
+  if (!result) {
+    return res.status(NOT_FOUND).json({ message: 'Produto não encontrado' });
+  }
+
+  return res.status(HTTP_OK).json(result);
+};
+
+module.exports = { getAll, getById };
