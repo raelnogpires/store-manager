26be113e129f84ef74a6dc7dd5791d386207784a
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-31 12:39:55 -0300

    fix: queries in salesModel are ok now

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,12 +1,31 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [result] = await connection.execute('SELECT * FROM StoreManager.sales_products;');
+  const query = `SELECT
+    s.id AS salesId,
+    s.date,
+    sp.product_id AS productId,
+    sp.quantity
+  FROM StoreManager.sales AS s
+  INNER JOIN StoreManager.sales_products AS sp
+  ON sp.sale_id = s.id
+  ORDER BY s.id, sp.product_id`;
+
+  const [result] = await connection.execute(query);
   return result;
 };
 
 const getById = async (id) => {
-  const query = 'SELECT * FROM StoreManager.sales WHERE id = ?;';
+  const query = `SELECT
+    s.date,
+    sp.product_id AS productId,
+    sp.quantity
+    FROM StoreManager.sales AS s
+  INNER JOIN StoreManager.sales_products AS sp
+  ON sp.sale_id = s.id
+  WHERE s.id = ?
+  ORDER BY sp.product_id`;
+
   const [result] = await connection.execute(query, [id]);
   return result;
 };
